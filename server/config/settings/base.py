42f52cbdbc3b5db442444459a8e25e74fffc7aac
Matt Nickerson <matt.nickerson993@gmail.com>
2022-02-24 20:28:42 -0500

    docker setup

--- server/config/settings/base.py
+++ server/config/settings/base.py
@@ -142,6 +142,7 @@ STATICFILES_FINDERS = [
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = Path(BASE_DIR / 'media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
