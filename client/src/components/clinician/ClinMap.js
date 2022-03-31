3d2ff1b1fda0d39d41584b4c12ca6e01559fa336
Matt Nickerson <matt.nickerson993@gmail.com>
2022-03-31 08:08:13 -0400

    map work

--- client/src/components/clinician/ClinMap.js
+++ client/src/components/clinician/ClinMap.js
@@ -1,15 +1,28 @@
 import React from "react";
 import {
+  DirectionsRenderer,
   DirectionsService,
+  DistanceMatrixService,
   GoogleMap,
   LoadScript,
   Marker,
 } from "@react-google-maps/api";
 
 const ClinMap = ({ patient_address, lat, lng, zoom }) => {
+  const [response, setResponse] = React.useState(null);
+  const [distResponse, setDistResponse] = React.useState(null);
   const clinician_address = { lat, lng };
-  console.log(patient_address);
-  console.log(clinician_address);
+  const directionsCallback = (response) => {
+    if (response !== null && response.status === "OK") {
+      setResponse(response);
+    }
+  };
+  const distanceCallback = (res) => {
+    console.log("res", res);
+    if (res !== null && res.status === "OK") {
+      setDistResponse(res);
+    }
+  };
   return (
     <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY}>
       <GoogleMap
@@ -24,17 +37,34 @@ const ClinMap = ({ patient_address, lat, lng, zoom }) => {
         }}
         zoom={zoom}
       >
-        {patient_address ? (
+        {/* {patient_address && (
           <DirectionsService
             options={{
-              origin: "Charlottesville, VA",
+              origin: { lat, lng },
               destination: patient_address,
               travelMode: "DRIVING",
             }}
+            callback={directionsCallback}
           />
-        ) : (
-          <Marker label="A" position={{ lat, lng }} />
         )}
+        {patient_address && response !== null && (
+          <DirectionsRenderer
+            options={{
+              directions: response,
+            }}
+          />
+        )} */}
+        {patient_address && (
+          <DistanceMatrixService
+            options={{
+              origins: [{ lat, lng }],
+              destinations: [patient_address],
+              travelMode: "DRIVING",
+            }}
+            callback={(res) => distanceCallback(res)}
+          />
+        )}
+        {!patient_address && <Marker label="A" position={{ lat, lng }} />}
       </GoogleMap>
     </LoadScript>
   );
